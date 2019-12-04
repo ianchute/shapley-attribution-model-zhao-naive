2d87b3a6699fdd3fbe778985786c8e7a2c7b07d1
Ian Chu Te <ianchute@hotmail.com>
2019-12-04 13:01:10 +0800

    Simplified Shapley performance improvements.

--- simplified_shapley_attribution_model.py
+++ simplified_shapley_attribution_model.py
@@ -1,26 +1,19 @@
 from itertools import chain, combinations
 from tqdm import tqdm
+from collections import Counter
 
 
 class SimplifiedShapleyAttributionModel:
-    def __init__(self):
-        self.P = set()
-
     def powerset(self, x):
         s = list(x)
         return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
-    def _r(self, S):
-        return sum(
-            [1 if S == journey else 0 for journey in self.indexed_journeys[len(S)]]
-        )
-
     def _phi(self, channel_index):
-        S_all = [set(S) for S in self.P_power if channel_index in S]
+        S_channel = [k for k in self.journeys.keys() if channel_index in k]
         score = 0
         print(f"Computing phi for channel {channel_index}...")
-        for S in tqdm(S_all):
-            score += self._r(S) / len(S)
+        for S in tqdm(S_channel):
+            score += self.journeys[S] / len(S)
         print(f"Attribution score for channel {channel_index}: {score:.2f}")
         print()
         return score
@@ -29,12 +22,10 @@ class SimplifiedShapleyAttributionModel:
         self.P = set(chain(*journeys))
         print("Running Simplified Shapley Attribution Model...")
         print(f"Found {len(self.P)} unique channels!")
-        self.P_power = list(self.powerset(self.P))
-        self.journeys = [set(journey) for journey in journeys]
-        self.indexed_journeys = {
-            i: [S for S in self.journeys if len(S) == i]
-            for i in range(1, len(self.P) + 1)
-        }
-        print(f"Proceeding to attribution computation...")
+        
+        print("Computing journey statistics...")
+        self.journeys = Counter([frozenset(journey) for journey in journeys])
+        
+        print(f"Computing attributions...")
         print()
         return {j: self._phi(j) for j in self.P}
